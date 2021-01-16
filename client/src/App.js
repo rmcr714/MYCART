ecb61a6ce8daf2857f2c6d163908c1baa540c3b8
Anurag Tiwari <tiwari136@gmail.com>
2021-01-16 23:25:03 +0530

    User redirected registration page completed

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Home from './pages/Home'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import Header from './components/nav/Header'
+import RegisterComplete from './pages/auth/RegisterComplete'
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path='/' component={Home} exact />
         <Route path='/login' component={Login} exact />
         <Route path='/register' component={Register} exact />
+        <Route path='/register/complete' component={RegisterComplete} exact />
       </Switch>
     </Router>
   )
