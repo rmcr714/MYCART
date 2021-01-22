a80668d59c76ce8efc988d106c81b24ca7505211
Anurag Tiwari <tiwari136@gmail.com>
2021-01-23 00:39:15 +0530

    Update password functionality in user side nav

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import RegisterComplete from './pages/auth/RegisterComplete'
 import ForgotPassword from './pages/auth/ForgotPassword'
 import History from './pages/user/History'
 import UserRoute from './components/routes/UserRoute'
+import Password from './pages/user/Password'
+import Wishlist from './pages/user/Wishlist'
 
 import { auth } from './firebase'
 import { useDispatch } from 'react-redux'
@@ -59,6 +61,8 @@ const App = () => {
         <Route path='/register/complete' component={RegisterComplete} exact />
         <Route path='/forgot/password' component={ForgotPassword} exact />
         <UserRoute path='/user/history' component={History} exact />
+        <UserRoute path='/user/password' component={Password} exact />
+        <UserRoute path='/user/wishlist' component={Wishlist} exact />
       </Switch>
     </Router>
   )
---
+++ client/src/pages/user/Password.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import UserNav from '../../components/nav/UserNav'
+import { auth } from '../../firebase'
+import { toast } from 'react-toastify'
+
+const Password = () => {
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+    await auth.currentUser
+      .updatePassword(password)
+      .then(() => {
+        setLoading(false)
+        setPassword('')
+        toast.success('password successfully updated')
+      })
+      .catch((err) => {
+        setLoading(false)
+        toast.error(err.message)
+      })
+  }
+
+  const passwordUpdateForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className='form-group'>
+        <label>Your Password</label>
+        <input
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          className='form-control'
+          placeholder='Enter new Password'
+          value={password}
+          disabled={loading}
+        />
+        <button
+          className='btn btn-primary'
+          disabled={!password || password.length < 6 || loading}
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  )
+
+  return (
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-md-2'>
+          <UserNav />
+        </div>
+        <div className='col-md-4 p-3'>
+          {loading ? (
+            <h3 className='text-danger'>Loading...</h3>
+          ) : (
+            <h3>Password Update</h3>
+          )}
+          {passwordUpdateForm()}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Password
