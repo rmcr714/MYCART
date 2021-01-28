a0ff205bb53212a34a4069af8100579c9d262c44
Anurag Tiwari <tiwari136@gmail.com>
2021-01-28 23:58:31 +0530

    backend Created Product Schema

---
+++ server/models/productModel.js
@@ -0,0 +1,73 @@
+import mongoose from 'mongoose'
+const { ObjectId } = mongoose.Schema
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+      text: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxlength: 2000,
+      text: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+      maxlength: 32,
+    },
+    category: {
+      type: ObjectId,
+      ref: 'Category',
+    },
+    subs: [
+      {
+        type: ObjectId,
+        ref: 'Sub',
+      },
+    ],
+    quantity: Number,
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    images: { type: Array },
+    shipping: {
+      type: String,
+      enum: ['Yes', 'No'],
+    },
+    color: {
+      type: String,
+      enum: ['Black', 'brown', 'silver', 'white', 'blue'],
+    },
+    brand: {
+      type: String,
+      enum: ['Samsung', 'Microsoft', 'Apple', 'Leovo', 'ASUS'],
+    },
+    // ratings: [
+    //   {
+    //     star: Number,
+    //     postedBy: { type: ObjectId, ref: 'User' },
+    //   },
+    // ],
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Product = mongoose.model('Product', productSchema)
+export default Product
